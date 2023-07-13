7ec0a2d26e6635cb195c43cd2a14b463059e835c
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-07-13 19:36:16 +0530

    button added

--- components/Header/Sidebar.tsx
+++ components/Header/Sidebar.tsx
@@ -5,33 +5,55 @@ import {
   AiOutlineSearch,
   AiOutlineWallet,
 } from "react-icons/ai";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card } from "../ui/card";
 const Sidebar: React.FC = () => {
   return (
     <>
-      <div className="flex mt-10 ">
-        <ul className="flex items-center flex-col w-40  py-10 shadow-md shadow-black-light">
-          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
-            <AiOutlineHome className="mr-4 w-6 h-6" />
-            Home
+      <Card className="flex mt-8 w-48 ">
+        <ul className="flex items-center flex-col w-40 py-10">
+          <li className="flex mb-6">
+            <Button variant="ghost">
+              <div className="flex items-center">
+                <AiOutlineHome className="w-6 h-6 mr-2.5" />
+                <span >Home</span>
+              </div>
+            </Button>
           </li>
-          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
-            <AiOutlineSearch className="mr-4 w-6 h-6" />
-            Search
+          <li className="flex mb-6">
+            <Button variant="ghost">
+              <div className="flex items-center">
+                <AiOutlineSearch className="w-6 h-6 mr-2.5" />
+                <span >Search</span>
+              </div>
+            </Button>
           </li>
-          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
-            <AiOutlineWallet className="mr-4 w-6 h-6" />
-            Create
+          <li className="flex mb-6">
+            <Button variant="ghost">
+              <div className="flex items-center">
+                <AiOutlineWallet className="w-6 h-6 mr-2.5" />
+                <span>Create</span>
+              </div>
+            </Button>
           </li>
-          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
-            <AiOutlineHeart className="mr-4 w-6 h-6" />
-            Activity
+          <li className="flex mb-6">
+            <Button variant="ghost">
+              <div className="flex items-center">
+                <AiOutlineHeart className="w-6 h-6 ml-2 mr-2" />
+                <span >Activity</span>
+              </div>
+            </Button>
           </li>
-          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
-            <AiOutlineUser className="mr-4 w-6 h-6" />
-            Profile
+          <li className="flex mb-6">
+            <Button variant="ghost">
+              <div className="flex items-center ">
+                <AiOutlineUser className="w-6 h-6 mr-2.5" />
+                <span>Profile</span>
+              </div>
+            </Button>
           </li>
         </ul>
-      </div>
+      </Card>
     </>
   );
 };
