16141ff341feaa940a17263303d4b39d441c8cde
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-07-13 06:51:16 +0530

    increased margin in sidebar li

--- components/Header/Sidebar.tsx
+++ components/Header/Sidebar.tsx
@@ -10,23 +10,23 @@ const Sidebar: React.FC = () => {
     <>
       <div className="flex mt-10 ">
         <ul className="flex items-center flex-col w-40  py-10 shadow-md shadow-black-light">
-          <li className="flex rounded-lg py-4 px-6 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
+          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
             <AiOutlineHome className="mr-4 w-6 h-6" />
             Home
           </li>
-          <li className="flex rounded-lg py-4 px-6 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
+          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
             <AiOutlineSearch className="mr-4 w-6 h-6" />
             Search
           </li>
-          <li className="flex rounded-lg py-4 px-6 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
+          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
             <AiOutlineWallet className="mr-4 w-6 h-6" />
             Create
           </li>
-          <li className="flex rounded-lg py-4 px-6 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
+          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
             <AiOutlineHeart className="mr-4 w-6 h-6" />
             Activity
           </li>
-          <li className="flex rounded-lg py-4 px-6 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
+          <li className="flex rounded-lg py-4 px-6 mb-2 font-bold align-middle cursor-pointer hover:text-gray-500 hover:bg-slate-100 ">
             <AiOutlineUser className="mr-4 w-6 h-6" />
             Profile
           </li>
