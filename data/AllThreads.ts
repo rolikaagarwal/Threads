8a072c79a1cfc6378b32c4818fb1f521c43ab483
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-07-16 04:52:00 +0530

    added caption and usernames

--- data/AllThreads.ts
+++ data/AllThreads.ts
@@ -10,25 +10,25 @@ export const fetchAllThreads = async (): Promise<AllThreads[]> => {
       userName: "rolika_agarwal",
       id: "1",
       profile_pic: "",
-      caption:"random data 1 "
+      caption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati praesentium cupiditate accusamus fuga ullam laboriosam beatae! Cupiditate dolorem vitae voluptatem blanditiis quae provident ipsum aliquam facere libero ut saepe quas dolor, odio quia voluptate eos excepturi similique, exercitationem incidunt voluptates molestias expedita hic cum! Accusamus et delectus praesentium, consectetur perspiciatis esse doloribus rerum voluptatibus illo quaerat. Quasi provident dolor ea amet ex quod tenetur delectus doloremque, quae beatae corporis accusamus nobis tempore. Excepturi in voluptatibus repellendus illo voluptatum ducimus eius animi nesciunt quod dolores sequi veniam, ad perspiciatis labore cum, consequuntur, libero quibusdam suscipit impedit unde illum ipsam consectetur! "
     },
     {
-      userName: "dummy_user",
+      userName: "kartikayasija",
       id: "2",
       profile_pic: "",
-      caption:"random data 2 "
+      caption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati praesentium cupiditate accusamus fuga ullam laboriosam beatae! Cupiditate dolorem vitae voluptatem blanditiis quae provident ipsum aliquam facere libero ut saepe quas dolor, odio quia voluptate eos excepturi similique, exercitationem incidunt voluptates molestias expedita hic cum! Accusamus et delectus praesentium, consectetur perspiciatis esse doloribus rerum voluptatibus illo quaerat. Quasi provident dolor ea amet ex quod tenetur delectus doloremque, quae beatae corporis accusamus nobis tempore. Excepturi in voluptatibus repellendus illo voluptatum ducimus eius animi nesciunt quod dolores sequi veniam, ad perspiciatis labore cum, consequuntur, libero quibusdam suscipit impedit unde illum ipsam consectetur!"
     },
     {
-      userName: "dummy_user2",
+      userName: "tanisha_agarwal",
       id: "3",
       profile_pic: "",
-      caption:"random data 1 "
+      caption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati praesentium cupiditate accusamus fuga ullam laboriosam beatae! Cupiditate dolorem vitae voluptatem blanditiis quae provident ipsum aliquam facere libero ut saepe quas dolor, odio quia voluptate eos excepturi similique, exercitationem incidunt voluptates molestias expedita hic cum! Accusamus et delectus praesentium, consectetur perspiciatis esse doloribus rerum voluptatibus illo quaerat. Quasi provident dolor ea amet ex quod tenetur delectus doloremque, quae beatae corporis accusamus nobis tempore. Excepturi in voluptatibus repellendus illo voluptatum ducimus eius animi nesciunt quod dolores sequi veniam, ad perspiciatis labore cum, consequuntur, libero quibusdam suscipit impedit unde illum ipsam consectetur! "
     },
     {
-      userName: "dummy_user3",
+      userName: "Swastik123",
       id: "4",
       profile_pic: "",
-      caption:"random data 1 "
+      caption:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati praesentium cupiditate accusamus fuga ullam laboriosam beatae! Cupiditate dolorem vitae voluptatem blanditiis quae provident ipsum aliquam facere libero ut saepe quas dolor, odio quia voluptate eos excepturi similique, exercitationem incidunt voluptates molestias expedita hic cum! Accusamus et delectus praesentium, consectetur perspiciatis esse doloribus rerum voluptatibus illo quaerat. Quasi provident dolor ea amet ex quod tenetur delectus doloremque, quae beatae corporis accusamus nobis tempore. Excepturi in voluptatibus repellendus illo voluptatum ducimus eius animi nesciunt quod dolores sequi veniam, ad perspiciatis labore cum, consequuntur, libero quibusdam suscipit impedit unde illum ipsam consectetur!"
     },
   ];
   return data;
