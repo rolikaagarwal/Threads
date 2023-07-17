ebd01fe8001a05e7d2b97748dc259e1a0f9a93f8
Kartikay Asija <kartikay7057@gmail.com>
2023-07-18 01:18:42 +0530

    prisma setup + get threads + create thread

--- app/api/thread/route.ts
+++ app/api/thread/route.ts
@@ -1,12 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "@/prisma";
+import { NextResponse } from "next/server";
+import prisma from "@/prisma";
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const GET = async (req: Request) => {
   console.log(req.method);
   try {
     const allThread = await prisma.thread.findMany({});
+    if (!allThread)
+      return NextResponse.json(
+        { Error: "Error fetching Threads" },
+        { status: 400 }
+      );
     return NextResponse.json(
       { message: "Success", allThread },
       { status: 200 }
@@ -16,3 +19,21 @@ export const GET = async (req: Request, res: NextResponse) => {
   }
 };
 
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    if (!body) {
+      return NextResponse.json({ Error: "No thread" }, { status: 400 });
+    }
+    const { caption, userName } = body;
+    const newThread = await prisma.thread.create({
+      data: { caption, userName },
+    });
+    if (!newThread) {
+      return NextResponse.json({ Error: "Error in Posting" }, { status: 400 });
+    }
+    return NextResponse.json(newThread, { status: 200 });
+  } catch (err) {
+    return NextResponse.json("Internal Server Error", { status: 400 });
+  }
+};
